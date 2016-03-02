d63594b6e14d7e277690992a277305b959023a3b
jalestro <jalestro@users.noreply.github.com>
2016-03-02 22:36:27 +0000

    added decode function to Cipher class (and it theoretically works)

--- classdef.py
+++ classdef.py
@@ -39,10 +39,10 @@ class Cipher:
     self.message = ""
     self.keyword = ""
     self.output = ""
-    self._createDic()
 
-#creates a dictionary with values in the form "input char": "output char"
-  def _createDic(self) :
+#creates a dictionary with values in the form "input char": "output char" for encoding a cipher
+#see _createDecodeDic for dictionary for decoding
+  def _createEncodeDic(self) :
     if (self.cipher == "C") :
       self.replace = {x: y for x in self.__vigtable[0] for y in self.__vigtable[offset]}
     elif (self.cipher == "A") :
@@ -53,19 +53,58 @@ class Cipher:
       print "The cipher code supplied is invalid. Please use only 'C' 'A' 'N' or 'V'"
     return
 
+#creates a dictionary with valuse in the form of "input char": "output char" for decoding a cipher
+#the dictionary created will be the inverse of that created by _createEncodeDic()
+#for example, in a Caesar cipher with offset 3, _createEncodeDic()'s first entry would be {'A': 'D'}
+#whereas _createDecodeDic()'s first entry would be {'D': 'A'}
+#given that dictionaries are unsorted "first entry" above refers to the first entry created by the function
+  def _createDecodeDic(self) :
+    if (self.cipher == "C") :
+      self.replace = {x: y for x in self.__vigtable[offset] for y in self.__vigtable[0]}
+    elif (self.cipher == "A") :
+      self.replace = {x: y for x in self.__atbash for y in self.__vigtable[0]} 
+    elif (self.cipher == "N") :
+      self.replace = {x: y for x in self.__num for y in self.__vigtable[0]}
+    elif (self.cipher == "V") :
+      self.replace = {x: y for x in self.__num for y in self.__vigtable[0] AND for x in self.__vigtable[0] for y in self.__num}
+    else :
+      print "The cipher code supplied is invalid. Please use only 'C' 'A' 'N' or 'V'"
+    return
+
   def encode(self, message, keyword = "NONE") :
     self.message = message.upper() #make sure message is all uppercase
     self.keyword = keyword.upper() #make sure keyword is all uppercase
+    self._createEncodeDic() #create the necessary encoding dictionary
     if (self.cipher == "V") :
       #DO THE COMPLICATED VIGENERE THING
-      self.__repeat()
-      for letter in self.message :
-        self.output += self.__vigtable[self.replace[letter]][
+      self.__repeat() #repeats keyword such that it is the same length as message
+      for index in range(len(self.message)) :
+        self.output += self.__vigtable[self.replace[self.message[index]]][self.replace[self.keyword[index]]]
     else :
       for letter in self.message :
         self.output += self.replace[letter]
     return
 
+  def decode(self, message, keyword = "NONE") :
+    self.message = message.upper() #if message is letters, ensure it is all uppercase
+    self.keyword = keyword.upper() #make sure keyword is all uppercase
+    self._createDecodeDic()
+    if (self.cipher == "V") :
+      self.__repeat() #repeat keyword such that it is the same length as the encoded message
+      for index in range(len(self.message)) :
+        thisTable = self.__vigtable[self.replace[self.keyword[index] - 1]] #pull the row from vigTable that corresponds to the current letter in the keyword
+        #then convert the tuple thisTable into a string so that we can use the string.find function
+        thisString = ""
+        for jndex in range(len(thisTable)) :
+          thisString += thisTable[jndex]
+        self.output += self.replace[thisString.find(self.message[index]) + 1]
+    else :
+      for char in self.message :
+        self.output += self.replace[char]
+    return
+    
+
+  #repeats keyword such that it is the same length as message
   def __repeat(self) :
     self.keyword = (self.keyword * int(math.ceil(float(len(self.message)) / len(self.keyword))))[:len(self.message)]
     return
